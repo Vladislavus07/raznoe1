1516d2e7aa9b0bb2a1eabd6fe0ac0bd4df745a7b
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-10-25 12:25:05 +0300

    Update ioqwufpiaguj.cpp

---
+++ ioqwufpiaguj.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+long long itc_len_a(string a)
+{
+	int z = 0;
+	for (int m = 0; str[m] != '\0'; m++)
+	{
+        	z++;
+	}
+	return z;
+}
+long long itc_len_b(string b)
+{
+	int z = 0;
+	for (int m = 0; str[m] != '\0'; m++)
+	{
+        	z++;
+	}
+	return z;
+}
+
+string itc_sloce(string a, string b)
+{
+    string n,i=0;
+    while (itc_len_a > i && itc_len_b > i)
+    {
+        n=a+b;
+        
+    }
+}
+
+int main()
+{
+    
+}
